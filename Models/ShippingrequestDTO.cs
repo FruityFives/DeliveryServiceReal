361eec83d3389b2bf1401c11554d9225f55333f5
Fawad Nassery <eaa23fana@students.eaaa.dk>
2025-04-10 12:12:49 +0200

    Commit

--- Models/ShippingrequestDTO.cs
+++ Models/ShippingrequestDTO.cs
@@ -2,6 +2,9 @@ namespace WorkerService.Models;
 
 public class ShippingrequestDTO
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
     public string CustomerName { get; set; }
     public string PickupAddress { get; set; }
     public string PackageId { get; set; } = Guid.NewGuid().ToString();
